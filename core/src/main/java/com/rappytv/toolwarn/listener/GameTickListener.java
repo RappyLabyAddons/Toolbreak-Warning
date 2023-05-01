d4b71ed73deb610a4f41e35205043a9e4ff4d796
RappyTV <rappytv@rappytv.com>
2023-05-01 16:11:49 +0200

    fixed closing GUIs directly when holding item with warning

--- core/src/main/java/com/rappytv/toolwarn/listener/GameTickListener.java
+++ core/src/main/java/com/rappytv/toolwarn/listener/GameTickListener.java
@@ -7,12 +7,14 @@ import com.rappytv.toolwarn.util.Util;
 import net.labymod.api.Laby;
 import net.labymod.api.client.entity.player.ClientPlayer;
 import net.labymod.api.client.entity.player.GameMode;
+import net.labymod.api.client.gui.screen.widget.overlay.ScreenOverlay;
 import net.labymod.api.client.world.item.ItemStack;
 import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.client.lifecycle.GameTickEvent;
 import net.labymod.api.util.I18n;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GameTickListener {
 
@@ -41,6 +43,8 @@ public class GameTickListener {
 
     public void toolUsed(ItemStack itemStack, ToolType toolType) {
         if(toolType == ToolType.None) return;
+        if(Laby.labyAPI().minecraft().minecraftWindow().isScreenOpened()) return;
+
         int itemWarnInt = (toolType.getWarnPercentage(config) * itemStack.getMaximumDamage()) / 100;
         int itemUsedInt = itemStack.getMaximumDamage() - itemStack.getCurrentDamageValue();
         if(config.debug().get())
