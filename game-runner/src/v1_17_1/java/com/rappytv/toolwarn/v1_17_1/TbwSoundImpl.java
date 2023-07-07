4ebbdc8d6e657f45222ec61ab8caf2925367e9e0
RappyTV <rappytv@rappytv.com>
2023-07-07 20:35:00 +0200

    Implement sounds for 1.17.1

---
+++ game-runner/src/v1_17_1/java/com/rappytv/toolwarn/v1_17_1/TbwSoundImpl.java
@@ -0,0 +1,30 @@
+package com.rappytv.toolwarn.v1_17_1;
+
+import com.rappytv.toolwarn.util.ITbwSounds;
+import net.labymod.api.client.resources.ResourceLocation;
+import net.labymod.api.models.Implements;
+import org.jetbrains.annotations.NotNull;
+
+import javax.inject.Singleton;
+
+@Singleton
+@Implements(ITbwSounds.class)
+public class TbwSoundImpl implements ITbwSounds {
+
+    @Override
+    public ResourceLocation getPlingSound() {
+        return ResourceLocation.create("minecraft", "block.note_block.pling");
+    }
+    @Override
+    public ResourceLocation getLevelUpSound() {
+        return ResourceLocation.create("minecraft", "entity.player.levelup");
+    }
+    @Override
+    public @NotNull ResourceLocation getGlassBreakSound() {
+        return ResourceLocation.create("minecraft", "block.glass.break");
+    }
+    @Override
+    public ResourceLocation getAnvilUseSound() {
+        return ResourceLocation.create("minecraft", "block.anvil.use");
+    }
+}
