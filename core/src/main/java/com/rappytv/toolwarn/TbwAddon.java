1cfccb0b495ff327d8d72a8a3519236230764df0
RappyTV <rappytv@rappytv.com>
2023-07-07 18:11:08 +0200

    Implement sound api into addon added default sound for testing

--- core/src/main/java/com/rappytv/toolwarn/TbwAddon.java
+++ core/src/main/java/com/rappytv/toolwarn/TbwAddon.java
@@ -1,7 +1,10 @@
 package com.rappytv.toolwarn;
 
 import com.rappytv.toolwarn.commands.TbwCommand;
+import com.rappytv.toolwarn.core.generated.DefaultReferenceStorage;
 import com.rappytv.toolwarn.listener.GameTickListener;
+import com.rappytv.toolwarn.util.ITbwSounds;
+import com.rappytv.toolwarn.util.WarnSound;
 import net.labymod.api.addon.LabyAddon;
 import net.labymod.api.models.addon.annotation.AddonMain;
 
@@ -9,7 +12,15 @@ import net.labymod.api.models.addon.annotation.AddonMain;
 public class TbwAddon extends LabyAddon<TbwConfiguration> {
 
     public static final String prefix = "§c§lTBW §8» §7";
+    public static WarnSound sound;
+    private static ITbwSounds sounds;
     private static TbwAddon instance;
+
+    @Override
+    protected void load() {
+        sounds = ((DefaultReferenceStorage) this.referenceStorageAccessor()).iTbwSounds();
+        sound = WarnSound.Pling;
+    }
 
     @Override
     protected void enable() {
@@ -22,6 +33,10 @@ public class TbwAddon extends LabyAddon<TbwConfiguration> {
 
     public static TbwAddon get() {
         return instance;
+    }
+
+    public static ITbwSounds getSounds() {
+        return sounds;
     }
 
     @Override
