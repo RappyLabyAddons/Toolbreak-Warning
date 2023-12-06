bcda3141528caf48c88b379a52bbc220790e83ba
RappyTV <rappytv@rappytv.com>
2023-12-06 22:46:41 +0100

    Bump labygradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "toolwarn"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.29"
+    val labyGradlePluginVersion = "0.3.38"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
