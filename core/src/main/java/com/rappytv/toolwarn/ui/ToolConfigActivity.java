b60cdb53a5c2628039cc003dbf837cb2f429e7c6
RappyTV <rappytv@rappytv.com>
2024-02-01 13:43:31 +0100

    Link config activity to lss file

--- core/src/main/java/com/rappytv/toolwarn/ui/ToolConfigActivity.java
+++ core/src/main/java/com/rappytv/toolwarn/ui/ToolConfigActivity.java
@@ -1,8 +1,10 @@
 package com.rappytv.toolwarn.ui;
 
 import net.labymod.api.client.gui.screen.activity.AutoActivity;
+import net.labymod.api.client.gui.screen.activity.Link;
 import net.labymod.api.client.gui.screen.activity.types.SimpleActivity;
 
+@Link("config.lss")
 @AutoActivity
 public class ToolConfigActivity extends SimpleActivity {
 
