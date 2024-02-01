62ec83607540fe35505dafc04f079a6442f879ab
RappyTV <rappytv@rappytv.com>
2024-02-01 13:05:54 +0100

    Add empty tool config activity

---
+++ core/src/main/java/com/rappytv/toolwarn/ui/ToolConfigActivity.java
@@ -0,0 +1,9 @@
+package com.rappytv.toolwarn.ui;
+
+import net.labymod.api.client.gui.screen.activity.AutoActivity;
+import net.labymod.api.client.gui.screen.activity.types.SimpleActivity;
+
+@AutoActivity
+public class ToolConfigActivity extends SimpleActivity {
+
+}
