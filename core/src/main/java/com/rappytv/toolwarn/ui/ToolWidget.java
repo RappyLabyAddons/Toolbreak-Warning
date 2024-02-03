f2701d3cc17cc9e3ac8c281f757f600a9ef7a9a0
RappyTV <rappytv@rappytv.com>
2024-02-03 17:58:46 +0100

    Add own tool spritesheet, fix ToolWidget meta line

--- core/src/main/java/com/rappytv/toolwarn/ui/ToolWidget.java
+++ core/src/main/java/com/rappytv/toolwarn/ui/ToolWidget.java
@@ -1,16 +1,13 @@
 package com.rappytv.toolwarn.ui;
 
 import com.rappytv.toolwarn.util.WarnTool;
-import net.labymod.api.Laby;
-import net.labymod.api.client.gui.icon.Icon;
 import net.labymod.api.client.gui.lss.property.annotation.AutoWidget;
 import net.labymod.api.client.gui.screen.Parent;
-import net.labymod.api.client.gui.screen.activity.Link;
 import net.labymod.api.client.gui.screen.widget.SimpleWidget;
 import net.labymod.api.client.gui.screen.widget.widgets.ComponentWidget;
 import net.labymod.api.client.gui.screen.widget.widgets.renderer.IconWidget;
+import net.labymod.api.util.I18n;
 
-@Link("tool.lss")
 @AutoWidget
 public class ToolWidget extends SimpleWidget {
 
@@ -23,18 +20,18 @@ public class ToolWidget extends SimpleWidget {
     @Override
     public void initialize(Parent parent) {
         super.initialize(parent);
-        IconWidget iconWidget = new IconWidget(Icon.head(Laby.labyAPI().getUniqueId()))
+        IconWidget iconWidget = new IconWidget(tool.getType().getIcon())
             .addId("tool-icon");
 
         ComponentWidget nameWidget = ComponentWidget.text(tool.getType().name())
             .addId("name-component");
 
-        ComponentWidget meta = ComponentWidget.i18n(
+        ComponentWidget meta = ComponentWidget.text(I18n.translate(
             "toolwarn.gui.meta",
-                tool.getWarnAt(),
-                tool.openChat() ? "§a✔§r" : "§c✘§r",
-                tool.lastHitWarn() ? "§a✔§r" : "§c✘§r"
-        ).addId("meta-component");
+            tool.getWarnAt(),
+            tool.openChat() ? "§a✔§r" : "§c✘§r",
+            tool.lastHitWarn() ? "§a✔§r" : "§c✘§r"
+        )).addId("meta-component");
 
         this.addChild(iconWidget);
         this.addChild(nameWidget);
