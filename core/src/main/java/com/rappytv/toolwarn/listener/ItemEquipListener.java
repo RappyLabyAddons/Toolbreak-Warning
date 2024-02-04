1a026836aec0da24f9ec27dd0870f069985e008b
RappyTV <rappytv@rappytv.com>
2024-02-04 15:12:32 +0100

    Add some event boilerplate code in ItemEquipListener

--- core/src/main/java/com/rappytv/toolwarn/listener/ItemEquipListener.java
+++ core/src/main/java/com/rappytv/toolwarn/listener/ItemEquipListener.java
@@ -1,9 +1,16 @@
 package com.rappytv.toolwarn.listener;
 
+import com.rappytv.toolwarn.TbwAddon;
 import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.client.entity.player.ClientHotbarSlotChangeEvent;
 
 public class ItemEquipListener {
+
+    private final TbwAddon addon;
+
+    public ItemEquipListener(TbwAddon addon) {
+        this.addon = addon;
+    }
 
     @Subscribe
     public void onItemEquip(ClientHotbarSlotChangeEvent event) {
