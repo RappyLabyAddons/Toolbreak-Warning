8b5b28f1b1c7624cfcbc085668896ddb02ae2df7
RappyTV <rappytv@rappytv.com>
2024-06-17 21:08:52 +0200

    Register 1.20.5 and 1.20.6, switch to production release

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,9 @@ labyMod {
                 "1.19.4",
                 "1.20.1",
                 "1.20.2",
-                "1.20.4"
+                "1.20.4",
+                "1.20.5",
+                "1.20.6"
         ) { version, provider ->
             configureRun(provider, version)
         }
@@ -47,7 +49,7 @@ labyMod {
     }
 
     addonDev {
-        snapshotRelease()
+        productionRelease()
     }
 }
 
