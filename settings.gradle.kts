c45a1810d4c0f8418cc79d8f23cd83f486f0ac7c
RappyTV <rappytv@rappytv.com>
2026-06-04 14:45:38 +0200

    sync addon template

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,11 @@
 rootProject.name = "toolwarn"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.5.7"
+    val labyGradlePluginVersion = "0.8.1"
 
     buildscript {
         repositories {
-            maven("https://dist.labymod.net/api/v1/maven/release/")
+            maven("https://maven.laby.net/api/v1/maven/release/")
             maven("https://maven.neoforged.net/releases/")
             maven("https://maven.fabricmc.net/")
             gradlePluginPortal()
